9b67727719e9e04db348480103cf3d09f5286c57
steven <taysteven111@gmail.com>
2019-08-22 13:20:15 +0700

    first commit

---
+++ MiniChallenge3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiniChallenge3
+//
+//  Created by Steven on 8/22/19.
+//  Copyright © 2019 Steven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
