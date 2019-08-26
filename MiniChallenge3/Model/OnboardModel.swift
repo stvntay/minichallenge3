d5deaf49404e0fa965acc2f4e021469e28958ff4
steven <taysteven111@gmail.com>
2019-08-26 13:56:21 +0700

    set ui

--- MiniChallenge3/Model/OnboardModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  OnboardModel.swift
-//  MiniChallenge3
-//
-//  Created by Steven on 8/26/19.
-//  Copyright © 2019 Steven. All rights reserved.
-//
-
-import Foundation
---
+++ MiniChallenge3/View/DoctorOnBoardView.swift
@@ -0,0 +1,32 @@
+//
+//  DoctorOnBoardView.swift
+//  MiniChallenge3
+//
+//  Created by Steven on 8/26/19.
+//  Copyright © 2019 Steven. All rights reserved.
+//
+
+import UIKit
+
+class DoctorOnBoardView: UIView {
+
+    @IBOutlet weak var doctorNameInput: UITextField!
+    @IBOutlet weak var hospitalNameInput: UITextField!
+    @IBOutlet weak var verifCodeInput: UITextField!
+    @IBOutlet weak var nextPageBtn: UIButton!
+    
+    
+    override func awakeFromNib() {
+        doctorNameInput.clearButtonMode = UITextField.ViewMode.whileEditing
+        hospitalNameInput.clearButtonMode = UITextField.ViewMode.whileEditing
+        verifCodeInput.clearButtonMode = UITextField.ViewMode.whileEditing
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
